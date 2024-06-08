444fffee63d8cf0bcfbe5925a6c0d7650caeabae
hzyycom <hzyycom@163.com>
2024-06-08 12:07:01 +0800

    优化排序

--- upload_image.py
+++ upload_image.py
@@ -3,20 +3,16 @@ from werkzeug.utils import secure_filename
 import os
 from PIL import Image
 from io import BytesIO
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'youzi'
-#base_image_path = '/var/www/html/youzi/'
 base_image_path = '%s/static/images/' %  app.root_path 
 
-#<div style="width: 50%; margin-left: auto; margin-right: auto;">
-#@app.route('/')
-#def index():
-#    return render_template('index.html', images=[])
- 
+
 @app.route('/upload/<name>', methods=['POST'])
 def upload_image(name):
-    print('upload name: ', name)
+    #print('upload name: ', name)
     if 'image' not in request.files:
         flash('No file part')
         return redirect(request.url)
@@ -28,26 +24,19 @@ def upload_image(name):
  
     if file:
         filename = secure_filename(file.filename)
+        hash_filename = hash(filename)
+        post = filename.split('.')
+
+        filename = getTime()
+        if len(post)>1:
+            filename = filename + '.' + post[-1]
         file.save('%s/%s/%s'  % (  base_image_path, name, filename))
         return f'文件上传成功: {filename}'
 
 
-#@app.route('/<path:filename>')
-#def send_image(filename):
-#    print('send_image:' + filename)
-#    filename = filename.replace('images/', '')
-#    filename = filename.replace('videos/', '')
-#    print('result: ' + filename)
-#    return send_from_directory(base_image_path, filename)
-
-#@app.route('/uploads/images/<path:filename>')
-#def send_image(filename):
-#    print('image path send_image:' + filename)
-#    return send_from_directory(base_image_path, filename)
-
 @app.route('/images/<string:name>/<path:filename>')
 def send_image(name, filename):
-    print('image path send_image:' + filename)
+    #print('image path send_image:' + filename)
     filename = '%s/%s/%s' %  (base_image_path ,name, filename)
     byte_io = image_resize(filename, resize=8) 
 
@@ -56,7 +45,6 @@ def send_image(name, filename):
 
 @app.route('/video/<string:name>/<path:filename>')
 def send_video(filename, name):
-    print('video path send_image:' + filename, name)
     return send_from_directory( '%s/%s' %  (base_image_path ,name), filename)
 
 
@@ -68,22 +56,15 @@ def imagelist():
     print(host)
     prefix_host = host.split('.')[0]
     images = os.listdir(base_image_path + '/' + prefix_host)
-    for img in images:
-        print(img)
-    images_list = [img for img in images if  img.split('.')[-1].lower()  not in ['mov', 'mp4']]
-    videos_list = [img for img in images if  img.split('.')[-1].lower()  in ['mov', 'mp4']]
-    #images_list = images
-    #videos_list = []
-    return render_template('index.html', images=images_list, videos=videos_list, name=prefix_host)
+    images.sort()
+    images_list = [img for img in images if  img.split('.')[-1].lower()  not in ['mov', 'mp4'] and not img.startswith(".")]
+    videos_list = [img for img in images if  img.split('.')[-1].lower()  in ['mov', 'mp4'] and not img.startswith(".")]
+    return render_template('index.html', images=images_list[::-1], videos=videos_list[::-1], name=prefix_host)
 
 
 def image_resize(origin_image_path, resize):
-
     image = Image.open(origin_image_path)
-    
     width, height = image.size
-    #info = image._getexif()
-    #print("image info:", info)
 
     if hasattr(image, '_getexif'):
         exif_data = image._getexif()
@@ -99,7 +80,6 @@ def image_resize(origin_image_path, resize):
                 elif orientation == 8:
                     image = image.transpose(Image.ROTATE_90)
     
-    print(int(width/resize), int(height/resize))
     # 缩放图片
     resized_image = image.resize((int(width/resize), int(height/resize)))
 
@@ -108,6 +88,11 @@ def image_resize(origin_image_path, resize):
     resized_image.save(byte_io, format='JPEG')
     byte_io.seek(0)
     return byte_io
+
+def getTime():
+    now = datetime.now()
+    return str(now.date()) + 'T' + str(now.time()).replace(':','-').replace('.', 'T')
+
 
 if __name__ == '__main__':
     app.run(port=8866)
